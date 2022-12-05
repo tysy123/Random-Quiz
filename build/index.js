d4f1923bfe2e980e3d64296889ebd663b7f07575
hiep.nt <hiep.nt@ohmyhotel.com>
2022-12-05 10:16:18 +0700

    update

--- build/index.js
+++ build/index.js
@@ -18,7 +18,7 @@ const path = require('path');
 const app = express();
 const publicPath = path.join(__dirname, 'public');
 app.use(express.static(publicPath));
-app.listen(5000);
+app.listen("https://random-quiz-bacc.netlify.app/");
 
 app.get('', (_, res) => {
     res.sendFile(`${publicPath}/index.html`);
